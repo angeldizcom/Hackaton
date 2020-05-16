74cdefd65d261714ad8c652b3364ef0af177cd6c
angeldizcom <angeldiazgarcia90@gmail.com>
2020-05-16 12:33:13 +0200

    Fundamentos python Kata 3

--- FundamentosPyton/Kata3/main.py
+++ FundamentosPyton/Kata3/main.py
@@ -1,1 +1,18 @@
+"""
+salas de juegos
+preguntar la edad
+< 4años = gratis
+> 4 y < 18 son 5€
+> 18 son 10€
+"""
 print('Kata3')
+
+edad = input("Introduce tu edad: ")
+edad = int(edad)
+
+if(edad < 4 ):
+    print("La entrada es gratuita.")
+elif(edad >= 4 and edad <= 18):
+    print("El precio de la entrada son 5€")
+else:
+    print("El precio de la entrada son 10€")
