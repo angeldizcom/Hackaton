2bb43c2d342d5a825aeb9cbe87db81c3d3ccdc8e
angeldizcom <angeldiazgarcia90@gmail.com>
2020-05-16 11:46:26 +0200

    pre hackaton

---
+++ video pre Hackaton/main.py
@@ -0,0 +1,16 @@
+'''
+Esto es un adelanto en hackaton , comentario en bloque
+'''
+nombre = 'Angel Diaz'
+edad = 15
+esta_Trabajando = True # False
+dato = input ('Introducir un dato: ')
+
+print(dato)
+
+print(edad)
+
+if( edad >= 18 ):
+    print('mayor')
+else:
+    print('menor')
