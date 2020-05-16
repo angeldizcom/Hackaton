e39eddeebb6eae943183e43c9c33c645b304851f
angeldizcom <angeldiazgarcia90@gmail.com>
2020-05-16 12:50:27 +0200

    Fundamentos python Kata4,5,6

--- FundamentosPyton/Kata4/main.py
+++ FundamentosPyton/Kata4/main.py
@@ -1,1 +1,9 @@
+'''
+Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla la 
+cuenta atrás desde ese número hasta cero separados por comas.
+'''
 print('Kata4')
+
+n = int(input("Introduce un número entero positivo: "))
+for i in range(n, -1, -1):
+    print(i, end=", ")
